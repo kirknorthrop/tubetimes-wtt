e8b55b74239016dca6eafad16a7298f71b32c857
Kirk Northrop <kirk@krn.me.uk>
2017-04-13 19:05:31 +0100

    Update docs and licence date

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+Very important note:
+
+`brew install poppler`
+
+Do not install pdftohtml directly.
+
+
 `pdftohtml -f <first_page_of_timetables> -l <last_page_of_timetables> -xml <input pdf> <output xml>`
 
 `xlsfromxml.py <output xml>`
--- xlsfromxml.py
+++ xlsfromxml.py
@@ -4,7 +4,7 @@
 # London Underground Working Timetable Spreadsheet converter
 # Created as part of the tubetimes project at http://tubetim.es/
 #
-# Copyright (c) 2015 Kirk Northrop <kirk@krn.me.uk>
+# Copyright (c) 2013-2017 Kirk Northrop <kirk@krn.me.uk>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
